40d7aae247d4d55ec580e8c4fee3989e8f84bb20
ankur-gupta <ankur@perfectlyrandom.org>
2020-02-03 18:33:08 -0800

    Adding some tests and requirements.txt to run GitHub Action

---
+++ flicker/tests/test_default_constructor.py
@@ -0,0 +1,22 @@
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import division
+from builtins import range
+
+import pytest
+from pyspark.sql import SparkSession
+
+from flicker import FlickerDataFrame
+
+
+class TestDefaultConstructor:
+    spark = (SparkSession.builder
+             .appName('TestDefaultConstructor').getOrCreate())
+
+    def test_basic(self):
+        df = self.spark.createDataFrame([(_, _) for _ in range(5)],
+                                        'a INT, b INT')
+        fdf = FlickerDataFrame(df)
+        assert isinstance(fdf, FlickerDataFrame)
+        assert fdf.shape == (5, 2)
+        assert set(fdf.columns) == {'a', 'b'}
